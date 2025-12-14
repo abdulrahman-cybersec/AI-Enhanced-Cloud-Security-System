8052dfd1e7e099192d183761917b8c09c1356e32
Abdulrhman-sybersec <abdulrahmanhijam75@gmail.com>
2025-12-14 14:49:19 +0300

    Delete backend/backend directory

--- backend/backend/app.py
+++
@@ -1,9 +0,0 @@
-# app.py
-from ai_models.intrusion_detection import analyze_traffic
-
-def run_backend():
-    result = analyze_traffic(packet_size=1200, failed_logins=3)
-    print("Backend analysis result:", result)
-
-if __name__ == "__main__":
-    run_backend()
